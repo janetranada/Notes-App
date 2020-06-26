f06ce789386b4bea7c2a76fcf7179fea88ff9713
janetranada <jyranada@gmail.com>
2020-06-26 13:01:40 +0300

    input feature; save entry to notes array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import Header from './components/Header/Header';
 import CreateNote from './components/CreateNote/CreateNote';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes(prevNotes => [...prevNotes, newNote]);    
+  }
+
+  useEffect(() => {
+    console.log(notes);
+  }, [notes]);
+
   return (
     <div className="App">
       <Header />
-      <CreateNote />
+      <CreateNote handleAdd={addNote} />
     </div>
   );
 }
--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css'
 
-const CreateNote = () => {
+const CreateNote = (props) => {
+    const { handleAdd } = props;
+
+    const [note, setNote] = useState({
+        title: '',
+        details: ''
+    })
+
+    const handleInputChange = (event) => {
+        const {name, value} = event.target;
+        setNote(prevNote => ({...prevNote, [name]: value}));
+    }
+
+    const handleSubmit = (event) => {
+        handleAdd(note);
+        setNote({
+            title: '',
+            details: ''
+        });
+        event.preventDefault();
+    }
+
     return (
         <div>
             <form className='create'>
@@ -9,14 +30,18 @@ const CreateNote = () => {
                     name='title'
                     placeholder='Note title'
                     spellCheck='false'
+                    value={note.title}
+                    onChange={handleInputChange}
                 />
                 <textarea 
                     name='details'
                     placeholder='Write your note here...'
                     rows='4'
                     spellCheck='false'
+                    value={note.details}
+                    onChange={handleInputChange}
                 />
-                <button>Add</button>
+                <button onClick={handleSubmit}>Add</button>
             </form>
         </div>
     );
